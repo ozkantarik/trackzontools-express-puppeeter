8e43fa2dad9dc40c872a2789e1bb1a9ddc3c6000
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2019-02-20 14:48:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const puppeeter =  require('puppeteer');
+ 
+const app = express();
+ 
+app.get('/get-html', async (req, res) => {
+
+    const browser  = await puppeeter.launch({
+        headless:false,
+    });
+   const page =  await browser.newPage();
+
+    await page.goto(req.query.url);
+    await res.send({
+        'html' : await   page.evaluate(() => document.body.innerHTML) 
+    });
+
+    await browser.close();
+});
+ 
+app.listen(3333, () => {
+  console.log('Listening on port 8080');
+});
